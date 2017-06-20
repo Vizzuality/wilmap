647fbe35721611431ab2bc108d968b1f00e2daaa
Iván Román <chewuaka@gmail.com>
2017-06-20 09:04:26 +0200

    theme update

--- themes/wilmap_theme/dist/js/main.js
+++ themes/wilmap_theme/dist/js/main.js
@@ -565,12 +565,68 @@
           $(dom + ' .views-row a').each(function(i, item) {
               var target = $(item).attr('href').split('?')[1];
 
-              if ( target.indexOf(uri) != -1 ) {
+//console.log(uri + ' || ' + target + ' || ' + (uri.indexOf(target)));
+
+
+              if ( uri.indexOf(target) != -1 ) {
                 $(item).addClass('__active');
               }
           });
 
+          var isPhone = (App.Utils.isMobile.Phone() || App.Utils.isMobile.Phone( 'desktop' ));
+          if(isPhone) {
+            $(dom + ' .view-content').slick({
+              dots: false,
+              arrows: false,
+              infinite: false,
+              speed: 300,
+              variableWidth: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              swipeToSlide: true,
+              centerMode: true,
+              focusOnSelect: true
+            });
+
+            // Nav to active item
+            var active = parseInt($(dom + ' .views-row a.__active').parents('.slick-slide').attr('data-slick-index'));
+            $(dom + ' .view-content').slick('slickGoTo', active);
+          }
         }
+
+      },
+
+      /**
+       * Resize and orientation triggers
+       */
+      resizeAndOrientation: function() {
+
+        // Add inital responsive classes
+        App.Utils.AddResponsiveBodyClasses();
+        App.curResponsiveClass  = ( ( App.Utils.isMobile.Phone( 'desktop' ) )?
+                                  'responsive-mobile-phone':( App.Utils.isMobile.Tablet( 'desktop' ) )?
+                                  'responsive-mobile-tablet':'responsive-desktop');
+
+
+        // Resize trigger
+        $( window ).on( 'resize', function(){
+          App.Utils.AddResponsiveBodyClasses();
+          var nextResponsiveClass  = ( ( App.Utils.isMobile.Phone( 'desktop' ) )?
+                                     'responsive-mobile-phone':( App.Utils.isMobile.Tablet( 'desktop' ) )?
+                                     'responsive-mobile-tablet':'responsive-desktop');
+
+
+          // If Desktop resize, reload page for initialize mobile
+          if( App.Utils.isDesktop() && App.curResponsiveClass !== nextResponsiveClass ) {
+            location.reload();
+            console.log('Responsive Reloaded')
+          }
+        });
+
+        // Orientarion trigger
+        $( window ).on( "orientationchange", function() {
+          App.Utils.AddResponsiveBodyClasses();
+        });
 
       }
 
