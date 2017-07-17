27776fc4665715e8bbc133ce958961b477a327a0
Iván Román <chewuaka@gmail.com>
2017-07-17 09:23:30 +0200

    Style Map

--- themes/wilmap_theme/lib/src/partials/_main.base.application.js
+++ themes/wilmap_theme/lib/src/partials/_main.base.application.js
@@ -26,7 +26,7 @@
           L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
               attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
               maxZoom: 5,
-              id: 'mapbox.streets',
+              id: 'mapbox.light',
               accessToken: 'your.mapbox.access.token'
           }).addTo(mymap);
         }
