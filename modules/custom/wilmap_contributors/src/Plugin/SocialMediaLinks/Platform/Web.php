8fe0daa66e5ad54595944fe90d63ce9735d4ef2d
Jorge Maestre <jorge.maestre@netberry.es>
2017-07-28 20:06:42 +0200

    Social Media Link plugin validation fixed

--- modules/custom/wilmap_contributors/src/Plugin/SocialMediaLinks/Platform/Web.php
+++ modules/custom/wilmap_contributors/src/Plugin/SocialMediaLinks/Platform/Web.php
@@ -4,37 +4,43 @@ namespace Drupal\wilmap_contributors\Plugin\SocialMediaLinks\Platform;
 
 use Drupal\social_media_links\PlatformBase;
 use Drupal\Core\Form\FormStateInterface;
-use Egulias\EmailValidator\EmailValidator;
+# use core/lib/Drupal/Component/Utility/
 use Drupal\Core\Url;
+use Drupal\Component\Utility\UrlHelper;
 
 /**
  * Provides 'web' platform.
  *
  * @Platform(
  *   id = "web",
- *   name = @Translation("Web"),
+ *   name = @Translation("Web")
  * )
  */
-class Web extends PlatformBase {
+class Web extends PlatformBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getUrl() {
-    return Url::fromUri('http:' . $this->getValue());
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function validateValue(array &$element, FormStateInterface $form_state, array $form) {
-    if (!empty($element['#value'])) {
-      $validator = new EmailValidator();
-
-      if (!$validator->isValid($element['#value'], TRUE)) {
-        $form_state->setError($element, t('The entered web URL is not valid.'));
-      }
+    /**
+     * {@inheritdoc}
+     */
+    public function getUrl()
+    {
+        return Url::fromUri($this->getValue());
     }
-  }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function validateValue(
+      array &$element,
+      FormStateInterface $form_state,
+      array $form
+    ) {
+        if (!empty($element['#value'])) {
+            if (!UrlHelper::isValid($element['#value'], true)) {
+                $form_state->setError($element,
+                  t('The entered web URL is not valid.'));
+            }
+        }
+    }
 
 }
