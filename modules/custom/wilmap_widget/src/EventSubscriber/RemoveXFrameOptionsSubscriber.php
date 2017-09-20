3b539e29adbe57f09f90e0b3467c1e69d9b5200e
Jorge Maestre <jorge.maestre@netberry.es>
2017-09-20 18:21:27 +0200

    Iframes limited tu /widgets/* paths

--- modules/custom/wilmap_widget/src/EventSubscriber/RemoveXFrameOptionsSubscriber.php
+++ modules/custom/wilmap_widget/src/EventSubscriber/RemoveXFrameOptionsSubscriber.php
@@ -6,15 +6,25 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class RemoveXFrameOptionsSubscriber implements EventSubscriberInterface {
+class RemoveXFrameOptionsSubscriber implements EventSubscriberInterface
+{
 
-    public function RemoveXFrameOptions(FilterResponseEvent $event) {
-        $response = $event->getResponse();
-        $response->headers->remove('X-Frame-Options');
-    }
-
-    public static function getSubscribedEvents() {
+    public static function getSubscribedEvents()
+    {
         $events[KernelEvents::RESPONSE][] = array('RemoveXFrameOptions', -10);
         return $events;
+    }
+
+    public function RemoveXFrameOptions(FilterResponseEvent $event)
+    {
+
+        $current_path = \Drupal::service('path.current')->getPath();
+        $pathargs = explode('/', $current_path);
+
+        // Only allows iframes in  paths like /widgets/*
+        if ($pathargs[1] == 'widgets') {
+            $response = $event->getResponse();
+            $response->headers->remove('X-Frame-Options');
+        }
     }
 }
